11d927ff168f84be1800cdd6826fa0449428dc55
Talha007 <talhaansari61@gmail.com>
2023-04-01 00:50:05 +0530

    comment added

--- src/main/java/com/uhg/controller/UserController.java
+++ src/main/java/com/uhg/controller/UserController.java
@@ -10,7 +10,10 @@ import reactor.core.publisher.Mono;
 @RestController
 @RequestMapping(path = "v1")
 public class UserController {
-
+	/**
+	 *
+	 * @return Set of data
+	 */
 	@GetMapping(path = "user")
 	public Mono<ResponseEntity<String>> getUserDetails() {
 		return Mono.just(ResponseEntity.ok("Succes"));
